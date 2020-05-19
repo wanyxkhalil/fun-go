7a37b0f121911e985e4d5327b9a51a49d7a13494
wanyx <wanyuxuan@meihaofenqi.com>
2020-05-19 11:49:53 +0800

    ifconfig

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	gin.DisableConsoleColor()
+	os.Create("fun_go.log")
+	router := gin.Default()
+	router.GET("/go/ifconfig", func(c *gin.Context) {
+		c.String(200, c.ClientIP())
+	})
+	router.Run(":8001")
+}
